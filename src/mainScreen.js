ece333ddf4891230fbc7a04c1c4bfd547cbf46ce
RezedaM <rezeda.magizova@yandex.ru>
2023-01-20 21:16:12 +0300

    second page done,linters added

---
+++ src/mainScreen.js
@@ -0,0 +1,101 @@
+let template = {
+    tag: 'form',
+    cls: 'game_lvl',
+    content: [
+        {
+            tag: 'h4',
+            cls: 'game_lvl-title',
+            content: 'Выберите сложность',
+        },
+        {
+            tag: 'div',
+            cls: 'game_lvl-dif',
+            content: [
+                {
+                    tag: 'div',
+                    cls: 'game_lvl-inputs',
+                    content: [
+                        {
+                            tag: 'label',
+                            cls: 'game_lvl-label',
+                            attrs: {
+                                for: 1,
+                            },
+                            content: 1,
+                        },
+                        {
+                            tag: 'input',
+                            cls: 'game_lvl-input',
+                            attrs: {
+                                type: 'radio',
+                                id: 1,
+                                value: 1,
+                                name: 'lvl',
+                            },
+                        },
+                    ],
+                },
+                {
+                    tag: 'div',
+                    cls: 'game_lvl-inputs',
+                    content: [
+                        {
+                            tag: 'label',
+                            cls: 'game_lvl-label',
+                            attrs: {
+                                for: 2,
+                            },
+                            content: 2,
+                        },
+                        {
+                            tag: 'input',
+                            cls: 'game_lvl-input',
+                            attrs: {
+                                type: 'radio',
+                                id: 2,
+                                value: 2,
+                                name: 'lvl',
+                            },
+                        },
+                    ],
+                },
+                {
+                    tag: 'div',
+                    cls: 'game_lvl-inputs',
+                    content: [
+                        {
+                            tag: 'label',
+                            cls: 'game_lvl-label',
+                            attrs: {
+                                for: 3,
+                            },
+                            content: 3,
+                        },
+                        {
+                            tag: 'input',
+                            cls: 'game_lvl-input',
+                            attrs: {
+                                type: 'radio',
+                                id: 3,
+                                value: 3,
+                                name: 'lvl',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            tag: 'button',
+            cls: 'game_lvl-btn',
+            content: 'Старт',
+        },
+    ],
+}
+
+function mainScreen(container) {
+    container.innerHTML = ''
+    container.appendChild(templateEngine(template))
+}
+
+window.application.screens['mainScreen'] = mainScreen
